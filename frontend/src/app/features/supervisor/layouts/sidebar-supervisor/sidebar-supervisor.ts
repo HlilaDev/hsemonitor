a8dd1494a0e2935911bf2319bd946dafdf35ef11
sookbit <sookbitcom@gmail.com>
2026-04-22 11:15:05 +0100

    fix sidebar supervisor

--- frontend/src/app/features/supervisor/layouts/sidebar-supervisor/sidebar-supervisor.ts
+++ frontend/src/app/features/supervisor/layouts/sidebar-supervisor/sidebar-supervisor.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -15,31 +15,59 @@ type SidebarSection = {
   items: SidebarItem[];
 };
 
-
-
 @Component({
   selector: 'app-sidebar-supervisor',
-  imports: [RouterLink, RouterLinkActive , TranslateModule , CommonModule],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './sidebar-supervisor.html',
   styleUrl: './sidebar-supervisor.scss',
 })
 export class SidebarSupervisor {
-   sections: SidebarSection[] = [
+  sections: SidebarSection[] = [
     {
       titleKey: 'SIDEBAR.SECTIONS.MAIN',
       items: [
-        { labelKey: 'SIDEBAR.MENU.DASHBOARD', icon: 'bi bi-grid', route: '/agent', exact: true },
-        { labelKey: 'SIDEBAR.MENU.OBSERVATIONS', icon: 'bi bi-eye', route: '/agent/observations' },
-        { labelKey: 'SIDEBAR.MENU.MONITORING', icon: 'bi bi-cpu', route: '/agent/devices' },
+        {
+          labelKey: 'SIDEBAR.MENU.DASHBOARD',
+          icon: 'bi bi-grid',
+          route: '/supervisor',
+          exact: true,
+        },
+        {
+          labelKey: 'SIDEBAR.MENU.OBSERVATIONS',
+          icon: 'bi bi-eye',
+          route: '/supervisor/observations',
+        },
+        {
+          labelKey: 'SIDEBAR.MENU.MONITORING',
+          icon: 'bi bi-broadcast',
+          route: '/supervisor/notifications',
+        },
       ],
     },
     {
       titleKey: 'SIDEBAR.SECTIONS.HSE',
       items: [
-        { labelKey: 'SIDEBAR.MENU.INCIDENTS', icon: 'bi bi-exclamation-triangle', route: '/agent/incidents' },
-        { labelKey: 'SIDEBAR.MENU.AUDITS', icon: 'bi bi-clipboard-check', route: '/agent/audits' },
-                { labelKey: 'SIDEBAR.MENU.INSPECTIONS', icon: 'bi bi-clipboard-check', route: '/agent/inspections' },
-        { labelKey: 'SIDEBAR.MENU.TRAINING', icon: 'bi bi-mortarboard', route: '/agent/trainings' },
+        {
+          labelKey: 'SIDEBAR.MENU.INCIDENTS',
+          icon: 'bi bi-exclamation-triangle',
+          route: '/supervisor/incidents',
+        },
+        {
+          labelKey: 'SIDEBAR.MENU.AUDITS',
+          icon: 'bi bi-clipboard-check',
+          route: '/supervisor/audits',
+        },
+        {
+          labelKey: 'SIDEBAR.MENU.INSPECTIONS',
+          icon: 'bi bi-ui-checks-grid',
+          route: '/supervisor/inspections',
+        },
+        {
+          labelKey: 'SIDEBAR.MENU.TRAINING',
+          icon: 'bi bi-mortarboard',
+          route: '/supervisor/trainings',
+        },
       ],
     },
   ];
@@ -49,6 +77,6 @@ export class SidebarSupervisor {
     accent: 'Supervisor',
     subtitle: 'hseMonitor',
     logoSrc: 'assets/images/logo.png',
-    homeLink: '/super',
+    homeLink: '/supervisor',
   };
 }
