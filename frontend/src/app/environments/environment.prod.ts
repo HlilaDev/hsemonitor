22688b2550547368279550a649abb233093be007
sookbit <sookbitcom@gmail.com>
2026-05-21 14:35:07 +0100

    prod dockerfile + environment

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// frontend/src/app/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  baseUrl: 'https://hsemonitor.com',
+  apiBaseUrl: 'https://hsemonitor.com/api',
+  uploadUrl: 'https://hsemonitor.com/uploads',
+};
