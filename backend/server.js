0a3fe17b0f5560b0bf93c8606371e5e2f96e42de
sookbit <sookbitcom@gmail.com>
2026-05-21 13:10:13 +0100

    set 5000 as a port

--- backend/server.js
+++ backend/server.js
@@ -102,7 +102,7 @@ const server = http.createServer(app);
 
 initSocket(server);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
